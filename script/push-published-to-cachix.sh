d78c0dcaf19499627cb180806934f611ba37c66f
Brian Hicks <brian@brianthicks.com>
2021-02-12 17:30:15 -0600

    fix archive URL

--- script/push-published-to-cachix.sh
+++ script/push-published-to-cachix.sh
@@ -2,4 +2,4 @@
 #!nix-shell -i bash -p cachix
 set -euo pipefail
 
-cachix watch-exec nix-script nix-build https://github.com/BrianHicks/nix-script/archive/main.zip
+cachix watch-exec nix-script nix-build https://github.com/BrianHicks/nix-script/archive/main.tar.gz
