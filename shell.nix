8df75e1e559f7ab08e6641ddbe4ed4f47a209d9a
Brian Hicks <brian@brianthicks.com>
2021-03-12 09:20:20 -0600

    add ghcid to shell

--- shell.nix
+++ shell.nix
@@ -7,6 +7,12 @@ in with nixpkgs;
 pkgs.mkShell {
   inputsFrom =
     [ (haskellPackages.callCabal2nix "nix-script" ./nix-script { }).env ];
-  buildInputs =
-    [ niv.niv git cabal-install haskellPackages.ormolu haskellPackages.hlint ];
+  buildInputs = [
+    niv.niv
+    git
+    cabal-install
+    haskellPackages.ormolu
+    haskellPackages.hlint
+    haskellPackages.ghcid
+  ];
 }
