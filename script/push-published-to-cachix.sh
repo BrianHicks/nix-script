676f8e39ce704faedcb60171792e3a223d02fd6d
Brian Hicks <brian@brianthicks.com>
2021-03-12 11:06:27 -0600

    make push-published-to-cachix actually push

--- script/push-published-to-cachix.sh
+++ script/push-published-to-cachix.sh
@@ -2,4 +2,4 @@
 #!nix-shell -i bash -p cachix
 set -euo pipefail
 
-cachix watch-exec nix-script nix-build https://github.com/BrianHicks/nix-script/archive/main.tar.gz
+cachix push nix-script "$(nix-build https://github.com/BrianHicks/nix-script/archive/main.tar.gz)"
