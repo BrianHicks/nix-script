6932dcaa601daebbaf3dcbc485d5704ea6e5dccf
Brian Hicks <brian@brianthicks.com>
2021-03-16 12:30:41 -0500

    actually test something in test-program-name

--- nix-script/samples/test-program-name.hs
+++ nix-script/samples/test-program-name.hs
@@ -3,6 +3,16 @@
 #!build ghc -o $OUT_FILE $SCRIPT_FILE
 
 import System.Environment
+import System.Exit
 
 main :: IO ()
-main = putStrLn =<< getProgName
+main = do
+  putStr "Checking that I have the right program name...\t"
+  name <- getProgName
+  case name of
+    "test-program-name.hs" ->
+      putStrLn "success!"
+    anythingElse -> do
+      putStrLn "failure!"
+      putStrLn ("I expected `test-program-name.hs` but got " ++ show anythingElse)
+      exitFailure
