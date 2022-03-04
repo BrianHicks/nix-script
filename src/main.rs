77d311821453a8c4bfa26cf96098e5e7476d10eb
Brian Hicks <brian@brianthicks.com>
2022-03-04 15:12:05 -0600

    start a CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use anyhow::Result;
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[clap(version, trailing_var_arg = true)]
+struct Opts {
+    /// The script to run
+    script: PathBuf,
+
+    /// Args to pass on to the script
+    script_args: Vec<String>,
+}
+
+impl Opts {
+    fn run(&self) -> Result<()> {
+        println!("{:#?}", self);
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+
+    if let Err(err) = opts.run() {
+        eprintln!("{:?}", err);
+        std::process::exit(1)
+    }
 }
