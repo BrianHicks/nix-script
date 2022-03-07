8b8b0bdf6ea54cf0caafa3fc0742a71da404bcbc
Brian Hicks <brian@brianthicks.com>
2022-03-07 06:16:20 -0600

    parse stuff from the parser

--- src/directives/mod.rs
+++ src/directives/mod.rs
@@ -1,11 +1,11 @@
 mod parser;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use rnix::types::Root;
 
 #[derive(Debug)]
 pub struct Directives<'src> {
-    build_inputs: NixExpr<'src>,
+    build: NixExpr<'src>,
 }
 
 #[derive(Debug)]
@@ -16,6 +16,9 @@ pub struct NixExpr<'src> {
 
 impl<'src> Directives<'src> {
     pub fn parse(indicator: &str, source: &'src str) -> Result<Self> {
+        let parser = parser::Parser::new(indicator).context("could not construct a parser")?;
+        let _fields = parser.parse(source);
+
         anyhow::bail!("todo")
     }
 }
