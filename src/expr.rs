432e1734f653925867d50d012a4e51707f3890ca
Brian Hicks <brian@brianthicks.com>
2022-03-09 15:41:27 -0600

    parse to result in ::parse

--- src/expr.rs
+++ src/expr.rs
@@ -9,11 +9,14 @@ pub struct Expr {
 }
 
 impl Expr {
-    pub fn parse(source: &str) -> Self {
-        Self {
+    pub fn parse(source: &str) -> Result<Self> {
+        Ok(Self {
             raw: source.to_string(),
-            parsed: rnix::parse(source).node(),
-        }
+            parsed: rnix::parse(source)
+                .as_result()
+                .context("failed to parse the source")?
+                .node(),
+        })
     }
 
     pub fn parse_as_list(source: &str) -> Result<Vec<Self>> {
@@ -50,12 +53,26 @@ mod tests {
 
         #[test]
         fn equal_if_raw_is_equal() {
-            assert_eq!(Expr::parse("a"), Expr::parse("a"))
+            assert_eq!(Expr::parse("a").unwrap(), Expr::parse("a").unwrap())
         }
 
         #[test]
         fn unequal_if_raw_is_unequal() {
-            assert!(Expr::parse("a") != Expr::parse("b"))
+            assert!(Expr::parse("a").unwrap() != Expr::parse("b").unwrap())
+        }
+    }
+
+    mod parse {
+        use super::*;
+
+        #[test]
+        fn accepts_valid() {
+            assert!(Expr::parse("a").is_ok())
+        }
+
+        #[test]
+        fn rejects_invalid() {
+            assert!(Expr::parse("[").is_err())
         }
     }
 
