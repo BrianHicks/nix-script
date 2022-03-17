e5ad1f55c05e5e29f26fb379facd0e65de803cb8
Brian Hicks <brian@brianthicks.com>
2022-03-17 05:49:05 -0500

    pass the build command to derivation

--- src/derivation/mod.rs
+++ src/derivation/mod.rs
@@ -8,25 +8,28 @@ use std::fmt::{self, Display};
 use std::path::Path;
 
 #[derive(Debug)]
-pub struct Derivation<'path> {
+pub struct Derivation<'path, 'src> {
     inputs: Inputs,
 
     name: &'path str,
     src: &'path Path,
 
+    build_command: &'src str,
+
     build_inputs: BTreeSet<Expr>,
     runtime_inputs: BTreeSet<Expr>,
 }
 
-impl<'path> Derivation<'path> {
-    pub fn new(src: &'path Path) -> Result<Self> {
+impl<'path, 'src> Derivation<'path, 'src> {
+    pub fn new(src: &'path Path, build_command: &'src str) -> Result<Self> {
         Ok(Self {
             inputs: Inputs::from(vec![("pkgs".into(), Some("import <nixpkgs> { }".into()))]),
             name: src
                 .file_name()
                 .and_then(|name| name.to_str())
                 .context("could not determine derivation name from input path")?,
-            src: src,
+            src,
+            build_command,
             build_inputs: BTreeSet::new(),
             runtime_inputs: BTreeSet::new(),
         })
@@ -56,7 +59,7 @@ impl<'path> Derivation<'path> {
         }
     }
 }
-impl Display for Derivation<'_> {
+impl Display for Derivation<'_, '_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
         write!(
             f,
@@ -80,6 +83,19 @@ impl Display for Derivation<'_> {
             write!(f, " ];\n")?;
         }
 
+        write!(
+            f,
+            "  buildPhase = ''\n    SRC={}\n\n    mkdir bin\n    OUT=bin/{}\n\n",
+            self.src.display(),
+            self.name,
+        )?;
+        if self.build_command.is_empty() {
+            write!(f, "    echo build command is not set\n    exit 1\n")?;
+        } else {
+            write!(f, "    {}\n", self.build_command)?;
+        }
+        write!(f, "  '';\n")?;
+
         write!(f, "}}")
     }
 }
@@ -101,35 +117,21 @@ mod tests {
         #[test]
         fn empty() {
             let path: PathBuf = [".", "path", "to", "my", "cool-script"].iter().collect();
-            let derivation = Derivation::new(&path).unwrap();
+            let derivation = Derivation::new(&path, "mv $SRC $DEST".into()).unwrap();
 
             assert_no_errors(&derivation.to_string());
-
-            assert_eq!(
-                String::from(
-                    "{ pkgs ? import <nixpkgs> { } }:\n{\n  name = \"cool-script\";\n  src = ./path/to/my/cool-script;\n}" 
-                ),
-                derivation.to_string()
-            )
         }
 
         #[test]
         fn with_build_inputs() {
             let path = PathBuf::from("./X");
-            let mut derivation = Derivation::new(&path).unwrap();
+            let mut derivation = Derivation::new(&path, "mv $SRC $DEST".into()).unwrap();
             derivation.add_build_inputs(vec![
                 Expr::parse("jq").unwrap(),
                 Expr::parse("bash").unwrap(),
             ]);
 
             assert_no_errors(&derivation.to_string());
-
-            assert_eq!(
-                String::from(
-                    "{ bash ? pkgs.bash, jq ? pkgs.jq, pkgs ? import <nixpkgs> { } }:\n{\n  name = \"X\";\n  src = ./X;\n\n  buildInputs = [ bash jq ];\n}"
-                ),
-                derivation.to_string(),
-            )
         }
     }
 }
