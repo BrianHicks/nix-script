000717bb8233bd00f43b9c7ccc7b6e721db0aa90
Brian Hicks <brian@brianthicks.com>
2022-04-06 12:07:40 -0500

    put these in a module for nice grouping

--- tests/end_to_end.rs
+++ tests/end_to_end.rs
@@ -4,23 +4,27 @@ fn bin() -> Command {
     Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap()
 }
 
-#[test]
-fn hello_world_py() {
-    let assert = bin().arg("sample-scripts/hello-world.py").assert();
+mod sample_scripts {
+    use super::*;
 
-    assert.success().stdout("Hello, World!\n");
-}
+    #[test]
+    fn hello_world_py() {
+        let assert = bin().arg("sample-scripts/hello-world.py").assert();
 
-#[test]
-fn hello_world_hs() {
-    let assert = bin().arg("sample-scripts/hello-world.hs").assert();
+        assert.success().stdout("Hello, World!\n");
+    }
 
-    assert.success().stdout("Hello, World!\n");
-}
+    #[test]
+    fn hello_world_hs() {
+        let assert = bin().arg("sample-scripts/hello-world.hs").assert();
 
-#[test]
-fn jq_sh() {
-    let assert = bin().arg("sample-scripts/jq.sh").assert();
+        assert.success().stdout("Hello, World!\n");
+    }
 
-    assert.success().stdout("Hello, World!\n");
+    #[test]
+    fn jq_sh() {
+        let assert = bin().arg("sample-scripts/jq.sh").assert();
+
+        assert.success().stdout("Hello, World!\n");
+    }
 }
