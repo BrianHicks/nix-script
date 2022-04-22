ded8c3951067724f236103415119989897a2ae51
Brian Hicks <brian@brianthicks.com>
2022-04-22 15:51:04 -0500

    verify that we've got the right version and name while packaging

--- flake.nix
+++ flake.nix
@@ -11,17 +11,24 @@
         pkgs = import inputs.nixpkgs { inherit system; };
         naersk-lib = inputs.naersk.lib."${system}";
       in rec {
-        packages.nix-script = naersk-lib.buildPackage {
+        packages.nix-script = naersk-lib.buildPackage rec {
           name = "nix-script";
+          version = "2.0.0";
 
           root = ./.;
 
           buildInputs = [ pkgs.clippy ];
-          target = [ "nix-script" ];
+          target = [ name ];
 
           doCheck = true;
           checkPhase = ''
             cargo clippy -- --deny warnings
+
+            # make sure we've listed the right version here (we can't grab
+            # it from the manifest because we use a virtual manifest for the
+            # various targets.) If this test fails, fix by making the `version`
+            # attribute the same as the one in `nix-script/Cargo.toml`
+            grep -q -e 'version = "${version}"' ${name}/Cargo.toml
           '';
         };
 
