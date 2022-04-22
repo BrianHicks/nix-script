26ae438172d7ee3641c8028f9e9e4a7e83ad5b29
Brian Hicks <brian@brianthicks.com>
2022-04-22 15:23:00 -0500

    use nix-community/naersk

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nmattia/naersk";
+    naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
