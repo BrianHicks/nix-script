5044c47df89a21d79883298d3381bba87076d38b
Brian Hicks <brian@brianthicks.com>
2022-04-25 08:14:31 -0500

    build generically

--- flake.nix
+++ flake.nix
@@ -10,39 +10,50 @@
       let
         pkgs = import inputs.nixpkgs { inherit system; };
         naersk-lib = inputs.naersk.lib."${system}";
+
+        rustTarget = name: version:
+          naersk-lib.buildPackage {
+            inherit name;
+            inherit version;
+
+            root = ./.;
+
+            buildInputs = [ pkgs.clippy ];
+            target = [ name ];
+
+            doCheck = true;
+            checkPhase = ''
+              cargo clippy -- --deny warnings
+
+              # make sure we've listed the right version here (we can't grab
+              # it from the manifest because we use a virtual manifest for the
+              # various targets.) If this test fails, fix by making the `version`
+              # attribute the same as the one in `nix-script/Cargo.toml`
+              grep -q -e 'version = "${version}"' ${name}/Cargo.toml
+            '';
+          };
       in rec {
-        packages.nix-script-all = pkgs.symlinkJoin {
-          name = "nix-script-all";
-          paths = [ packages.nix-script packages.nix-script-bash ];
-        };
+        packages = {
+          nix-script-all = pkgs.symlinkJoin {
+            name = "nix-script-all";
+            paths = [
+              packages.nix-script
+              packages.nix-script-haskell
+              packages.nix-script-bash
+            ];
+          };
 
-        packages.nix-script = naersk-lib.buildPackage rec {
-          name = "nix-script";
-          version = "2.0.0";
+          nix-script = rustTarget "nix-script" "2.0.0";
 
-          root = ./.;
-
-          buildInputs = [ pkgs.clippy ];
-          target = [ name ];
-
-          doCheck = true;
-          checkPhase = ''
-            cargo clippy -- --deny warnings
-
-            # make sure we've listed the right version here (we can't grab
-            # it from the manifest because we use a virtual manifest for the
-            # various targets.) If this test fails, fix by making the `version`
-            # attribute the same as the one in `nix-script/Cargo.toml`
-            grep -q -e 'version = "${version}"' ${name}/Cargo.toml
+          nix-script-bash = pkgs.writeShellScriptBin "nix-script-bash" ''
+            exec ${packages.nix-script}/bin/nix-script \
+              --build-command 'cp $SRC $OUT' \
+              --interpreter bash \
+              "$@"
           '';
-        };
 
-        packages.nix-script-bash = pkgs.writeShellScriptBin "nix-script-bash" ''
-          exec ${packages.nix-script}/bin/nix-script \
-            --build-command 'cp $SRC $OUT' \
-            --interpreter bash \
-            "$@"
-        '';
+          nix-script-haskell = rustTarget "nix-script-haskell" "2.0.0";
+        };
 
         defaultPackage = packages.nix-script-all;
 
@@ -50,6 +61,7 @@
           nix-script-all = packages.nix-script-all;
           nix-script = packages.nix-script;
           nix-script-bash = packages.nix-script-bash;
+          nix-script-haskell = packages.nix-script-haskell;
         };
 
         devShell = pkgs.mkShell {
